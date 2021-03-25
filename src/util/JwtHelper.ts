a1148e1169cd876765884893a07174f40d2614b0
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2021-03-25 19:39:16 +0100

    created automatic iat and exp

--- src/util/JwtHelper.ts
+++ src/util/JwtHelper.ts
@@ -2,7 +2,11 @@ import { sign, verify } from "jsonwebtoken"
 import { JWT_BODY, JWT_CONFIG } from "../config/jwt_config"
 
 export function SignToken(payload: JWT_BODY) {
-    return sign(payload, JWT_CONFIG.JWT_SECRET)
+    return sign({
+        exp: Math.floor(Date.now()/1000) + JWT_CONFIG.EXPIRATION_TIME,
+        iat: Math.floor(Date.now()/1000),
+        ...payload
+    }, JWT_CONFIG.JWT_SECRET)
 }
 
 export function VerifyToken<T>(token: string) {
