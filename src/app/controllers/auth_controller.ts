36743f905a41c57ce80d854be0611e5fc4aa7ce2
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2021-03-25 12:16:21 +0100

    created mock login endpoint

--- src/app/controllers/auth_controller.ts
+++ src/app/controllers/auth_controller.ts
@@ -1,4 +1,19 @@
 import { Router } from "express"
+import { SignToken } from "../../util/JwtHelper"
+import { RouteHelper } from "../../util/RouteHelper"
 const router = Router()
+
+router.get("/login", RouteHelper({ private: true, roles: ["admin"] }, (req, res) => {
+
+    return res.json({
+        token: SignToken({
+            email: "johndoe@acme.com",
+            iat: Math.floor(Date.now() / 1000),
+            exp: Math.floor(Date.now() / 1000) + 3600,
+            roles: [ "admin" ]
+        })
+    })
+    
+}))
 
 export const auth_controller = router
