5d48e8182db9ef849b2edcda763758350307d6b9
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2021-03-25 10:32:12 +0100

    applied routes to server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,12 @@
 import express from "express"
+import { ROUTES_CONFIG } from "../config/routes_config"
 import { SERVER_CONFIG } from "../config/server_config"
 
 const app = express()
 
 app.listen(SERVER_CONFIG.PORT)
 app.use(SERVER_CONFIG.MIDDLEWARE)
+
+ROUTES_CONFIG.forEach(value => {
+    app.use(value.path, value.controller)
+})
