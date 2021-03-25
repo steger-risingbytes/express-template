160a859d969270628b047ee2f715c6e93da33ae3
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2021-03-25 19:39:06 +0100

    edited config

--- src/config/jwt_config.ts
+++ src/config/jwt_config.ts
@@ -1,12 +1,14 @@
 export const JWT_CONFIG = {
 
-    JWT_SECRET: String(process.env.JWT_SECRET || "ciscocisco")
+    JWT_SECRET: String(process.env.JWT_SECRET || "ciscocisco"),
 
+    EXPIRATION_TIME: 3600
 }
 
 export type JWT_BODY = {
-    iat: number
-    exp: number
+    iat?: number
+    exp?: number
+    user_id: string
     email: string
     roles: string[]
 }
